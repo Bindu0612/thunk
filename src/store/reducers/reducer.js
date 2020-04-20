9674e1ef4e649a6bbbdbdea9b5c11cf783345de3
Bindu0612 <58510359+Bindu0612@users.noreply.github.com>
2020-04-20 12:57:41 +0530

    ok

--- src/store/reducers/reducer.js
+++ src/store/reducers/reducer.js
@@ -6,6 +6,7 @@ const reducer = (state = initialState, action) => {
   const newState = { ...state };
 
   switch (action.type) {
+    
     case "AGE_UP":
       newState.age += action.value;
       newState.loading = false;
@@ -14,6 +15,7 @@ const reducer = (state = initialState, action) => {
     case "AGE_DOWN":
       newState.age -= action.value;
       break;
+
     case "LOADING":
       newState.loading = true;
   }
